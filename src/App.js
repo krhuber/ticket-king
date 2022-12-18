bdf4829de3b63b5a77578bac8e5306dc4d71f2da
krhuber <kr.huber@gmail.com>
2022-12-18 16:20:57 -0500

    Created Base React App
    Installed Bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Ticket King</header>
+    </div>
+  );
+}
+
+export default App;
